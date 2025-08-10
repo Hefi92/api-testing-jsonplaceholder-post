2a40123c7326ee0f5aa397f8b5a1ff25cf74fba0
Hefi <hofler.petr@gmail.com>
2025-08-10 12:13:18 +0200

    Updated version of get.py

--- tests/get.py
+++ tests/get.py
@@ -1,8 +1,9 @@
 import pytest 
 import requests
 
-def tests_api_status_code():
-  url = "https://jsonplaceholder.typicode.com/posts"
-  
-  response = requests.get(url)
-  assert response.status_code == 200
+
+
+def test_api_status_code():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    assert response.status_code == 200
